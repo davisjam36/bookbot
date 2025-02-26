742a7c1ccc23b90cc86a574de17123328fd24e5b
davisjt <davisjam18@gmail.com>
2025-02-25 23:59:52 -0500

    imported sys and changed file routing

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import sys
 from stats import (
     get_num_words,
     char_dict_to_sorted_list,
     get_char_dict,
 )
 
-
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_dict = get_char_dict(text)
